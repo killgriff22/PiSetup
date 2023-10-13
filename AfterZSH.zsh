fbbd373e70498bd3fcc6990cd34a6da32fb2cfc0
X-x <37548204+killgriff22@users.noreply.github.com>
2023-10-12 22:35:29 -0400

    Update AfterZSH.zsh

--- AfterZSH.zsh
+++ AfterZSH.zsh
@@ -2,3 +2,5 @@ git clone https://github.com/killgriff22/ATLoader
 cd ATLoader
 cp -rf *.py ../../.oh-my-zsh
 cp -r @themes
+sudo apt install -y python3-pip
+sudo pip install termcolor colorama discord.py
